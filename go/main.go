b76394645bd308a65cedff8c1a9b070dea46800b
Beksultan A <babdikaarov@gmail.com>
2024-08-15 02:54:46 +0300

    chore(production): gin conf

--- go/main.go
+++ go/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-    // gin.SetMode(gin.ReleaseMode) // production mode
+    gin.SetMode(gin.ReleaseMode) // production mode
      if err := godotenv.Load(); err != nil {
         log.Fatalf("Error loading .env file")
     }
